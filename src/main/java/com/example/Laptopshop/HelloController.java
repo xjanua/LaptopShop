3328795d35387c3945430200c38f724fa21191a1
xjanua <ktoanhutech@gmail.com>
2024-11-30 16:13:24 +0700

    update Driver Security

--- src/main/java/com/example/Laptopshop/HelloController.java
+++ src/main/java/com/example/Laptopshop/HelloController.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // localhost:8080
     @GetMapping("/")
     public String index() {
-        return "Hello Kim Toan Dep trai";
+        return "Hello Kim Toan Update";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 }
