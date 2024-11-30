7e50a6085c635f1a943ed647a8bdac22bffc3965
xjanua <ktoanhutech@gmail.com>
2024-11-30 13:02:01 +0700

    Push src

---
+++ src/main/java/com/example/Laptopshop/LaptopshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Laptopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Laptopshop/LaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
