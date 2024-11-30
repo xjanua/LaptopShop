b5b92a31cdd0c9efd7130b9f27a52d4537d99e80
xjanua <ktoanhutech@gmail.com>
2024-11-30 14:47:34 +0700

    update Code

---
+++ src/main/java/com/example/Laptopshop/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.Laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello Kim Toan Dep trai";
+    }
+}
--- src/main/java/com/example/Laptopshop/LaptopshopApplication.java
+++ src/main/java/com/example/Laptopshop/LaptopshopApplication.java
@@ -2,12 +2,17 @@ package com.example.Laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
-	}
+		// SpringApplication.run(LaptopshopApplication.class, args);
 
+		ApplicationContext HoidanIt = SpringApplication.run(LaptopshopApplication.class, args);
+		for (String s : HoidanIt.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
+	}
 }
