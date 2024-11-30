7e202fcd8991d63c78c44dbe0f12223e88cf291c
xjanua <ktoanhutech@gmail.com>
2024-11-30 23:30:53 +0700

    update

---
+++ src/main/java/com/example/Laptopshop/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.Laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Laptopshop.services.UserService;
+
+@Controller
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        String test = this.userService.handleHello();
+        return "hello";
+    }
+
+}
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
---
+++ src/main/java/com/example/Laptopshop/services/UserService.java
@@ -0,0 +1,10 @@
+package com.example.Laptopshop.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello() {
+        return "Hello from Service";
+    }
+}
