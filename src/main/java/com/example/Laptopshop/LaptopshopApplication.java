cb2d46481be52d1c14925fec4d14f4d37ae46355
xjanua <ktoanhutech@gmail.com>
2024-11-30 22:07:44 +0700

    update

--- src/main/java/com/example/Laptopshop/LaptopshopApplication.java
+++ src/main/java/com/example/Laptopshop/LaptopshopApplication.java
@@ -4,15 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(LaptopshopApplication.class, args);
+		SpringApplication.run(LaptopshopApplication.class, args);
 
-		ApplicationContext HoidanIt = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : HoidanIt.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
+		// ApplicationContext HoidanIt =
+		// SpringApplication.run(LaptopshopApplication.class, args);
+		// for (String s : HoidanIt.getBeanDefinitionNames()) {
+		// System.out.println(s);
+		// }
 	}
 }
