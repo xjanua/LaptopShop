8342b9397fbc757c443facac397e5b88b7ad90c2
xjanua <ktoanhutech@gmail.com>
2024-12-09 00:15:57 +0700

    update

--- src/main/java/com/example/Laptopshop/domain/Order.java
+++ src/main/java/com/example/Laptopshop/domain/Order.java
@@ -2,8 +2,6 @@ package com.example.Laptopshop.domain;
 
 import java.util.List;
 
-import org.hibernate.annotations.ManyToAny;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- src/main/java/com/example/Laptopshop/domain/OrderDetail.java
+++ src/main/java/com/example/Laptopshop/domain/OrderDetail.java
@@ -1,6 +1,5 @@
 package com.example.Laptopshop.domain;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
